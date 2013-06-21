ceec948299fbb144f980180c613dbae8bc4b2857
magomimmo <mimmo.cosenza@gmail.com>
2013-06-21 13:26:14 +0200

    still working

--- src/clj/modern_cljs/core.clj
+++ src/clj/modern_cljs/core.clj
@@ -2,7 +2,8 @@
   (:require [compojure.core :refer [defroutes GET POST]]
             [compojure.route :refer [resources not-found]]
             [compojure.handler :refer [site]]
-            [modern-cljs.login :refer [authenticate-user]]))
+            [modern-cljs.login :refer [authenticate-user]]
+            [modern-cljs.templates.shopping :refer [shopping] ]))
 
 ;; defroutes macro defines a function that chains individual route
 ;; functions together. The request map is passed to each function in
@@ -14,7 +15,7 @@
   (POST "/login" [email password] (authenticate-user email password))
   ;; to server shopping command
   (POST "/shopping" [quantity price tax discount]
-        (str "You enter: " quantity " " price " " tax " " discount "."))
+        (shopping quantity price tax discount))
   ;; to server static pages saved in resources/public directory
   (resources "/")
   ;; if page is not found
