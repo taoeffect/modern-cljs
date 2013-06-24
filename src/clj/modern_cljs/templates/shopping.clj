a604d73405403b7e6bed5d5edecc9298d1e5ae1e
magomimmo <mimmo.cosenza@gmail.com>
2013-06-25 00:12:51 +0200

    step-2

--- src/clj/modern_cljs/templates/shopping.clj
+++ src/clj/modern_cljs/templates/shopping.clj
@@ -1,5 +1,7 @@
 (ns modern-cljs.templates.shopping
-  (:require [net.cgrand.enlive-html :refer [deftemplate set-attr]]))
+  (:require [net.cgrand.enlive-html :refer [deftemplate]]))
 
 (deftemplate shopping "public/shopping.html"
-  [quantity price tax discount] nil nil)
+  [quantity price tax discount]
+  nil
+  nil)
