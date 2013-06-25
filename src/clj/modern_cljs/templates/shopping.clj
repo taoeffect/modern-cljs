085ba038032362d4a11da534258e814e08c7c514
magomimmo <mimmo.cosenza@gmail.com>
2013-06-25 09:15:03 +0200

    added template

--- src/clj/modern_cljs/templates/shopping.clj
+++ src/clj/modern_cljs/templates/shopping.clj
@@ -1,7 +1,9 @@
 (ns modern-cljs.templates.shopping
-  (:require [net.cgrand.enlive-html :refer [deftemplate]]))
+  (:require [net.cgrand.enlive-html :refer [deftemplate set-attr]]))
 
 (deftemplate shopping "public/shopping.html"
   [quantity price tax discount]
-  nil
-  nil)
+  [:#quantity] (set-attr :value quantity)
+  [:#price] (set-attr :value price)
+  [:#tax] (set-attr :value tax)
+  [:#discount] (set-attr :value discount))
