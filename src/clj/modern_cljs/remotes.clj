89052d292ea4f3243d19468a12e053133bd6b865
Giacomo Cosenza <giacomo.cosenza@sinapsi.com>
2013-06-25 15:45:33 +0200

    refactoring

--- src/clj/modern_cljs/remotes.clj
+++ src/clj/modern_cljs/remotes.clj
@@ -1,13 +1,18 @@
 (ns modern-cljs.remotes
   (:require [modern-cljs.core :refer [handler]]
             [modern-cljs.login.java.validators :as v]
+            [modern-cljs.utils :refer [parse-integer parse-double]]
             [compojure.handler :refer [site]]
             [shoreleave.middleware.rpc :refer [defremote wrap-rpc]]))
 
 (defremote calculate [quantity price tax discount]
-  (-> (* quantity price)
-      (* (+ 1 (/ tax 100)))
-      (- discount)))
+  (let [q (parse-integer quantity)
+        p (parse-double price)
+        t (parse-double tax)
+        d (parse-double discount)]
+  (-> (* q p)
+      (* (+ 1 (/ t 100)))
+      (- d)))
 
 (defremote email-domain-errors [email]
   (v/email-domain-errors email))
--- src/clj/modern_cljs/templates/shopping.clj
+++ src/clj/modern_cljs/templates/shopping.clj
@@ -1,9 +1,11 @@
 (ns modern-cljs.templates.shopping
-  (:require [net.cgrand.enlive-html :refer [deftemplate set-attr]]))
+  (:require [net.cgrand.enlive-html :refer [deftemplate set-attr]]
+            [modern-cljs.remotes :refer [calculate]]))
 
 (deftemplate shopping "public/shopping.html"
   [quantity price tax discount]
   [:#quantity] (set-attr :value quantity)
   [:#price] (set-attr :value price)
   [:#tax] (set-attr :value tax)
-  [:#discount] (set-attr :value discount))
+  [:#discount] (set-attr :value discount)
+  [:#total] (set-attr :value (calculate quantity price tax discount)))
